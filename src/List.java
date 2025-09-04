5a9a4148e60d4c7ea620d7438f4355282d5a7f5f
Alejandro Garcia Pelayo <alejandrogarciapelayo@gmail.com>
2025-09-04 10:12:21 -0600

    Limpieza

--- src/List.java
+++ src/List.java
@@ -1,68 +1,30 @@
-public class  List<E> {
-
+public class List<E> {
     Node<E> firstNode;
 
-    /**
-     * Constructors
-     */
-
     public List() {
-
         this.firstNode = null;
-
     }
 
-
-    public List(Node firstNode) {
-
+    public List(Node<E> firstNode) {
         this.firstNode = firstNode;
-
     }
 
-    /**
-     * Setters y getters
-     */
-
-    public Node getFirstNode() {
-
+    public Node<E> getFirstNode() {
         return firstNode;
-
     }
 
-    public void insertAtFirstPosition(int value) {
-
-        Node nodeToInsert = new Node(value);
-
+    public void insertAtFirstPosition(E value) {
+        Node<E> nodeToInsert = new Node<>(value);
         nodeToInsert.next = firstNode;
-
         firstNode = nodeToInsert;
-
     }
 
-    /**
-
-     * Muestra toda la lista
-
-     */
-
-    public void show(){
-
-        //Nodo que recorre la lista
-
-        Node currentNode= this.firstNode;
-
-        //Mientras que el nodo no haya encontrado el final
-
-        //muestra el valor del nodo actual
-
-        while (currentNode!= null){
-
-            System.out.print(currentNode.data+" ");
-
-            currentNode= currentNode.next;
-
+    public void show() {
+        Node<E> currentNode = this.firstNode;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
         }
-
+        System.out.println();
     }
-
 }
--- src/Node.java
+++ src/Node.java
@@ -1,24 +1,14 @@
-public class  Node<E> {
+public class Node<E> {
+    E data;
+    Node<E> next;
 
-    E  data;
-
-    Node next;
-
-
-    public Node(E data, Node next) {
-
+    public Node(E data, Node<E> next) {
         this.data = data;
-
         this.next = next;
-
     }
-
 
     public Node(E data) {
-
         this.data = data;
-
+        this.next = null;
     }
-
-
 }
